3f34e1bc5d27bdb6234d1947c164c57307556d0d
“Ricardo <pinedaricardo07@gmail.com>
2023-12-02 15:10:26 -0500

    initial commit: base project structure

---
+++ TOTAL_CAR_FEES.API/Controllers/PricesController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TOTAL_CAR_FEES.API.Controllers
+{
+
+    [ApiController]
+    [Route("api/prices")]
+    public class PricesController : ControllerBase
+	{
+		public PricesController()
+		{
+		}
+
+		[HttpGet("get-car-fees")]
+		public IActionResult GetCarPriceAndFees([FromQuery] string basePrice, [FromQuery] string vehicleType)
+		{
+			return Ok();
+		}
+	}
+}
+
---
+++ TOTAL_CAR_FEES.TEST/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace TOTAL_CAR_FEES.TEST;
+
+[TestClass]
+public class UnitTest1
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+    }
+}
